6379ff69631804934a074aa9551f843c93f6b469
Huy Nguyen <thanhhuya10lk@gmail.com>
2017-08-04 23:30:30 +0700

    Update popup.js

--- popup.js
+++ popup.js
@@ -7,16 +7,16 @@ function renderImage(imageData) {
   document.getElementById('image').src = imageData.fixed_height_small_url;
 }
 
-async function getBoobsUrl() {
-  let url = 'https://api.giphy.com/v1/gifs/random?api_key=a89c66e48519481ab448a3f8356e635c&tag=boobs';
+async function getYoonaUrl() {
+  let url = 'https://api.giphy.com/v1/gifs/random?api_key=14d9bfd07fc34221938cae36eb5aefdf&tag=yoona';
   let result = await fetch(url);
   let jsonResult = await result.json();
   return jsonResult.data;
 }
 
 document.addEventListener('DOMContentLoaded', async () => {
-  renderStatus('Đang tìm vếu ...');
-  var imageData = await getBoobsUrl();
+  renderStatus('Finding Yoona ...');
+  var imageData = await getYoonaUrl();
   renderStatus('');
   renderImage(imageData);
 });
